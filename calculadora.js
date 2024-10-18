a2a10a6f8016de72fceadfabb4c307b2cd4ee179
Patricia Grings <patriciagrings@hotmail.com>
2024-10-18 19:06:03 -0300

    atualização

--- calculadora.js
+++ calculadora.js
@@ -1,21 +1,49 @@
-var vetor = []
-var lerMostrador = document.getElementById('mostrador')
+try {
+    var vetor = []
+    var lerMostrador = document.getElementById('mostrador')
+    var resultadoMostrado = false
 
-function leitura(event) {
-    var valorClique = event.target.value
-    lerMostrador.innerHTML += valorClique
-    vetor.push(valorClique)
+
+    function leitura(event) {
+        var valorClique = event.target.value
+
+        if (resultadoMostrado && !isNaN(valorClique)) {
+            vetor.length = 0
+            lerMostrador.innerHTML = ''
+        }
+
+        if (valorClique == '/100') {
+            lerMostrador.innerHTML += '%'
+        }
+        else {
+            lerMostrador.innerHTML += valorClique
+        }
+        
+        vetor.push(valorClique)
+        resultadoMostrado = false
+    }
+
+    function calculo(){
+        try {
+            debugger
+            var expressao = vetor.join('')
+            var resultado = eval(expressao)
+            lerMostrador.innerHTML = resultado
+            vetor.length = 0
+            vetor.push(resultado)
+            resultadoMostrado = true
+        } catch (error) {
+            alert(error.toString())
+        }
+    }
+
+    function limpar(){
+        vetor.length = 0
+        lerMostrador.innerHTML = ''
+        resultadoMostrado = false
+    }
+} catch (error) {
+    alert(error.toString())
 }
 
-function calculo(){
-    var expressao = vetor.join('')
-    var resultado = eval(expressao)
-    lerMostrador.innerHTML = resultado
-    vetor.length = 0
-    vetor.push(resultado)
-}
 
-function limpar(){
-    vetor.length = 0
-    lerMostrador.innerHTML = ''
-}
